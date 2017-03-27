177aec6094a1e0857155138d136415d313b9ddb0
leonthemisfit <leonthemisfit@gmail.com>
2017-03-26 21:47:30 -0400

    Added basic message factory with essential IRC coommands

---
+++ irc/factory.rb
@@ -0,0 +1,29 @@
+module Factory
+  def Factory.pong(ping)
+    return "PONG " + ping
+  end
+
+  def Factory.pass(password)
+    return "PASS " + password
+  end
+
+  def Factory.user(nick, user)
+    return "USER " + nick + " NULL NULL :" + user
+  end
+
+  def Factory.nick(nick)
+    return "NICK " + nick
+  end
+
+  def Factory.quit(qmsg)
+    return "QUIT :" + qmsg
+  end
+
+  def Factory.join(chan)
+    return "JOIN " + chan
+  end
+
+  def Factory.privmsg(rec, msg)
+    return "PRIVMSG " + rec + " :" + msg
+  end
+end
