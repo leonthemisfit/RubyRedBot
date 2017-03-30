07a074705bf8cc82d4becc787ea5537984bfdd75
leonthemisfit <leonthemisfit@gmail.com>
2017-03-30 19:38:46 -0400

    Added new_message method to Message object so that a message can be reused with a new subject and body

--- mail/message.rb
+++ mail/message.rb
@@ -4,8 +4,14 @@ module Mail
   class Message
     def initialize(from, from_addr, to, to_addr, subj, body)
       @from = from_addr
+      @from_name = from
       @to = to_addr
+      @to_name = to
       @message = Mail::Factory.create(from, from_addr, to, to_addr, subj, body)
+    end
+
+    def new_message(subj, body)
+      @message = Mail::Factory.create(@from_name, @from, @to_name, @to, subj, body)
     end
 
     attr_accessor :from, :to, :message
